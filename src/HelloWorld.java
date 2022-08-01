ca8f5c7b88477a2efe0909c3165db2a4e8d2d7f8
shaquielle robbins <shaquiellerobbins@gmail.com>
2022-08-01 12:37:28 -0500

    changed title of java file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
-public class Helloworld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.print("");
     }
 }
